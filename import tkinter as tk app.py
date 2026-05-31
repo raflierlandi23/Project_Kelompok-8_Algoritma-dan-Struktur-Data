7fc20f47f316cf0dc953743a65f12468cc8f784d
Raja ngoding <raflierlandi23@gmail.comgit config --listgit config --global user.email raflierlandi23@gmail.com>
2026-05-31 14:09:05 +0700

    Refactor: Mengubah nama file berantakan menjadi app.py

---
+++ import tkinter as tk app.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from datetime import datetime
+
+class Book:
+    def __init__(self, isbn, title, author, status="Tersedia"):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.status = status
+
+class BookNode:
+    def __init__(self, book):
+        self.book = book
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+    def append(self, book):
+        new_node = BookNode(book)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        self.size += 1
+
+    def delete(self, isbn):
+        if not self.head:
+            return False
+        if self.head.book.isbn == isbn:
+            self.head = self.head.next
+            self.size -= 1
+            return True
+        current = self.head
+        while current.next:
+            if current.next.book.isbn == isbn:
+                current.next = current.next.next
+                self.size -= 1
+                return True
+            current = current.next
+        return False
+
+    def to_list(self):
+        result = []
+        current = self.head
+        while current:
+            result.append(current.book)
+            current = current.next
+        return result
+
