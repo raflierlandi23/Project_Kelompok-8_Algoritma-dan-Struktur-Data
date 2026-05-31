a603b44091ae74a7ba643363f5573a349f5eee1d
All <aaldiw1111@gmail.com>
2026-05-31 15:29:16 +0700

    struktur data tumpukan

--- import tkinter as tk app.py
+++ import tkinter as tk app.py
@@ -53,3 +53,21 @@ class LinkedList:
             current = current.next
         return result
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def to_list_reversed(self):
+        return list(reversed(self.items))
+
